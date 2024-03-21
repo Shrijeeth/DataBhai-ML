0e755442304b032035c127211ddcb9ba41d7b14c
shrijeethsuresh <shrijeethsuresh@gmail.com>
2024-03-21 21:08:53 +0000

    feat(text-to-sql): fix issues

--- rag/text_to_sql_rag.py
+++ rag/text_to_sql_rag.py
@@ -41,5 +41,26 @@ class TextToSqlRag:
         ]
         return self.vector_store.add_texts(requirements, metadata=metadata)
 
-    def get_similar_texts(self, query: str, top_k: int = 5):
-        if self.vector_store.num_results
+    def get_similar_texts(self, query: str, top_k_tables: int = 3, top_k_instructions: int = 5, top_k_requirements: int = 2):
+        tables = self.vector_store.hybrid_search(query, whereFilter={
+            "type": TextToSqlTypes.TABLE,
+        }, k=top_k_tables)
+        table_prompt = ''
+        for table in tables:
+            table_prompt += table.page_content + "\n\n"
+
+        instructions = self.vector_store.hybrid_search(query, whereFilter={
+            "type": TextToSqlTypes.INSTRUCTION,
+        }, k=top_k_instructions)
+        instruction_prompt = ''
+        for instruction in instructions:
+            instruction_prompt += instruction.page_content + "\n\n"
+
+        requirements = self.vector_store.hybrid_search(query, whereFilter={
+            "type": TextToSqlTypes.REQUIREMENT,
+        }, k=top_k_requirements)
+        requirement_prompt = ''
+        for requirement in requirements:
+            requirement_prompt += requirement.page_content + "\n\n"
+
+        return table_prompt, instruction_prompt, requirement_prompt
